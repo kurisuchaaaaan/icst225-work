d993c6d5cfaee70472e48ef8783f9cb5e2a97187
kurisuchaaaaan <kurisuchaaaaan@gmail.com>
2019-05-24 17:55:55 +0800

    working no challenge

--- pragata_ps3.py
+++ pragata_ps3.py
@@ -4,9 +4,6 @@ splitted_data = string_data.split(",")
 toprint = 0.00
 for elem in splitted_data:
     num = float(elem)
-    if elem == "" | elem.find(",,") | type(elem) != "float":
-        print("ERROR!")
     square = num * num
     toprint += square
-
 print(toprint)
